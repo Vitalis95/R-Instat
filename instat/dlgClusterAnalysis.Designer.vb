c7ba85661b87996feb0eac5145594364c683cf88
Vitalis95 <vitaliskwambai@gmail.com>
2021-11-12 13:58:38 +0300

    Adding controls to the dialog

--- instat/dlgClusterAnalysis.Designer.vb
+++ instat/dlgClusterAnalysis.Designer.vb
@@ -22,13 +22,207 @@ Partial Class dlgClusterAnalysis
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.rdoHierarchicalData = New System.Windows.Forms.RadioButton()
+        Me.rdoPartitioningData = New System.Windows.Forms.RadioButton()
+        Me.rdoNumericVariables = New System.Windows.Forms.RadioButton()
+        Me.rdoDataFrame = New System.Windows.Forms.RadioButton()
+        Me.ucrBase = New instat.ucrButtons()
+        Me.ucrReceiverClusterData = New instat.ucrReceiverMultiple()
+        Me.ucrSelectorClusterData = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrPnlClusterData = New instat.UcrPanel()
+        Me.ucrPnlSelectData = New instat.UcrPanel()
+        Me.ucrChkMetric = New instat.ucrCheck()
+        Me.ucrChkCluster = New instat.ucrCheck()
+        Me.ucrChkMethod = New instat.ucrCheck()
+        Me.ucrChkStand = New instat.ucrCheck()
+        Me.ucrInputMetric = New instat.ucrInputComboBox()
+        Me.ucrInputMethod = New instat.ucrInputComboBox()
+        Me.ucrNudPamCluster = New instat.ucrNud()
         Me.SuspendLayout()
+        '
+        'rdoHierarchicalData
+        '
+        Me.rdoHierarchicalData.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoHierarchicalData.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoHierarchicalData.FlatAppearance.BorderSize = 2
+        Me.rdoHierarchicalData.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoHierarchicalData.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoHierarchicalData.Location = New System.Drawing.Point(277, 8)
+        Me.rdoHierarchicalData.Name = "rdoHierarchicalData"
+        Me.rdoHierarchicalData.Size = New System.Drawing.Size(100, 28)
+        Me.rdoHierarchicalData.TabIndex = 2
+        Me.rdoHierarchicalData.TabStop = True
+        Me.rdoHierarchicalData.Text = "Hierarchical"
+        Me.rdoHierarchicalData.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoHierarchicalData.UseVisualStyleBackColor = True
+        '
+        'rdoPartitioningData
+        '
+        Me.rdoPartitioningData.Appearance = System.Windows.Forms.Appearance.Button
+        Me.rdoPartitioningData.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoPartitioningData.FlatAppearance.BorderSize = 2
+        Me.rdoPartitioningData.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.rdoPartitioningData.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.rdoPartitioningData.Location = New System.Drawing.Point(179, 8)
+        Me.rdoPartitioningData.Name = "rdoPartitioningData"
+        Me.rdoPartitioningData.Size = New System.Drawing.Size(100, 28)
+        Me.rdoPartitioningData.TabIndex = 3
+        Me.rdoPartitioningData.TabStop = True
+        Me.rdoPartitioningData.Text = "Partitioning"
+        Me.rdoPartitioningData.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.rdoPartitioningData.UseVisualStyleBackColor = True
+        '
+        'rdoNumericVariables
+        '
+        Me.rdoNumericVariables.AutoSize = True
+        Me.rdoNumericVariables.Location = New System.Drawing.Point(316, 54)
+        Me.rdoNumericVariables.Name = "rdoNumericVariables"
+        Me.rdoNumericVariables.Size = New System.Drawing.Size(110, 17)
+        Me.rdoNumericVariables.TabIndex = 5
+        Me.rdoNumericVariables.TabStop = True
+        Me.rdoNumericVariables.Text = "Numeric Variables"
+        Me.rdoNumericVariables.UseVisualStyleBackColor = True
+        '
+        'rdoDataFrame
+        '
+        Me.rdoDataFrame.AutoSize = True
+        Me.rdoDataFrame.Location = New System.Drawing.Point(316, 76)
+        Me.rdoDataFrame.Name = "rdoDataFrame"
+        Me.rdoDataFrame.Size = New System.Drawing.Size(113, 17)
+        Me.rdoDataFrame.TabIndex = 6
+        Me.rdoDataFrame.TabStop = True
+        Me.rdoDataFrame.Text = "Data Frame/Matrix"
+        Me.rdoDataFrame.UseVisualStyleBackColor = True
+        '
+        'ucrBase
+        '
+        Me.ucrBase.Location = New System.Drawing.Point(7, 381)
+        Me.ucrBase.Name = "ucrBase"
+        Me.ucrBase.Size = New System.Drawing.Size(410, 52)
+        Me.ucrBase.TabIndex = 9
+        '
+        'ucrReceiverClusterData
+        '
+        Me.ucrReceiverClusterData.frmParent = Me
+        Me.ucrReceiverClusterData.Location = New System.Drawing.Point(321, 105)
+        Me.ucrReceiverClusterData.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverClusterData.Name = "ucrReceiverClusterData"
+        Me.ucrReceiverClusterData.Selector = Nothing
+        Me.ucrReceiverClusterData.Size = New System.Drawing.Size(120, 100)
+        Me.ucrReceiverClusterData.strNcFilePath = ""
+        Me.ucrReceiverClusterData.TabIndex = 8
+        Me.ucrReceiverClusterData.ucrSelector = Nothing
+        '
+        'ucrSelectorClusterData
+        '
+        Me.ucrSelectorClusterData.bDropUnusedFilterLevels = False
+        Me.ucrSelectorClusterData.bShowHiddenColumns = False
+        Me.ucrSelectorClusterData.bUseCurrentFilter = True
+        Me.ucrSelectorClusterData.Location = New System.Drawing.Point(7, 26)
+        Me.ucrSelectorClusterData.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorClusterData.Name = "ucrSelectorClusterData"
+        Me.ucrSelectorClusterData.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorClusterData.TabIndex = 0
+        '
+        'ucrPnlClusterData
+        '
+        Me.ucrPnlClusterData.Location = New System.Drawing.Point(179, 2)
+        Me.ucrPnlClusterData.Name = "ucrPnlClusterData"
+        Me.ucrPnlClusterData.Size = New System.Drawing.Size(206, 39)
+        Me.ucrPnlClusterData.TabIndex = 4
+        '
+        'ucrPnlSelectData
+        '
+        Me.ucrPnlSelectData.Location = New System.Drawing.Point(295, 47)
+        Me.ucrPnlSelectData.Name = "ucrPnlSelectData"
+        Me.ucrPnlSelectData.Size = New System.Drawing.Size(170, 52)
+        Me.ucrPnlSelectData.TabIndex = 7
+        '
+        'ucrChkMetric
+        '
+        Me.ucrChkMetric.Checked = False
+        Me.ucrChkMetric.Location = New System.Drawing.Point(7, 220)
+        Me.ucrChkMetric.Name = "ucrChkMetric"
+        Me.ucrChkMetric.Size = New System.Drawing.Size(100, 20)
+        Me.ucrChkMetric.TabIndex = 10
+        '
+        'ucrChkCluster
+        '
+        Me.ucrChkCluster.Checked = False
+        Me.ucrChkCluster.Location = New System.Drawing.Point(8, 288)
+        Me.ucrChkCluster.Name = "ucrChkCluster"
+        Me.ucrChkCluster.Size = New System.Drawing.Size(100, 20)
+        Me.ucrChkCluster.TabIndex = 11
+        '
+        'ucrChkMethod
+        '
+        Me.ucrChkMethod.Checked = False
+        Me.ucrChkMethod.Location = New System.Drawing.Point(7, 265)
+        Me.ucrChkMethod.Name = "ucrChkMethod"
+        Me.ucrChkMethod.Size = New System.Drawing.Size(100, 20)
+        Me.ucrChkMethod.TabIndex = 12
+        '
+        'ucrChkStand
+        '
+        Me.ucrChkStand.Checked = False
+        Me.ucrChkStand.Location = New System.Drawing.Point(7, 243)
+        Me.ucrChkStand.Name = "ucrChkStand"
+        Me.ucrChkStand.Size = New System.Drawing.Size(100, 20)
+        Me.ucrChkStand.TabIndex = 13
+        '
+        'ucrInputMetric
+        '
+        Me.ucrInputMetric.AddQuotesIfUnrecognised = True
+        Me.ucrInputMetric.GetSetSelectedIndex = -1
+        Me.ucrInputMetric.IsReadOnly = False
+        Me.ucrInputMetric.Location = New System.Drawing.Point(127, 219)
+        Me.ucrInputMetric.Name = "ucrInputMetric"
+        Me.ucrInputMetric.Size = New System.Drawing.Size(137, 21)
+        Me.ucrInputMetric.TabIndex = 14
+        '
+        'ucrInputMethod
+        '
+        Me.ucrInputMethod.AddQuotesIfUnrecognised = True
+        Me.ucrInputMethod.GetSetSelectedIndex = -1
+        Me.ucrInputMethod.IsReadOnly = False
+        Me.ucrInputMethod.Location = New System.Drawing.Point(127, 263)
+        Me.ucrInputMethod.Name = "ucrInputMethod"
+        Me.ucrInputMethod.Size = New System.Drawing.Size(137, 21)
+        Me.ucrInputMethod.TabIndex = 15
+        '
+        'ucrNudPamCluster
+        '
+        Me.ucrNudPamCluster.DecimalPlaces = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudPamCluster.Increment = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.ucrNudPamCluster.Location = New System.Drawing.Point(126, 290)
+        Me.ucrNudPamCluster.Maximum = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.ucrNudPamCluster.Minimum = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.ucrNudPamCluster.Name = "ucrNudPamCluster"
+        Me.ucrNudPamCluster.Size = New System.Drawing.Size(50, 20)
+        Me.ucrNudPamCluster.TabIndex = 16
+        Me.ucrNudPamCluster.Value = New Decimal(New Integer() {0, 0, 0, 0})
         '
         'dlgClusterAnalysis
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(613, 450)
+        Me.ClientSize = New System.Drawing.Size(520, 450)
+        Me.Controls.Add(Me.ucrNudPamCluster)
+        Me.Controls.Add(Me.ucrInputMethod)
+        Me.Controls.Add(Me.ucrInputMetric)
+        Me.Controls.Add(Me.ucrChkStand)
+        Me.Controls.Add(Me.ucrChkMethod)
+        Me.Controls.Add(Me.ucrChkCluster)
+        Me.Controls.Add(Me.ucrChkMetric)
+        Me.Controls.Add(Me.ucrBase)
+        Me.Controls.Add(Me.ucrReceiverClusterData)
+        Me.Controls.Add(Me.rdoDataFrame)
+        Me.Controls.Add(Me.rdoNumericVariables)
+        Me.Controls.Add(Me.rdoPartitioningData)
+        Me.Controls.Add(Me.rdoHierarchicalData)
+        Me.Controls.Add(Me.ucrSelectorClusterData)
+        Me.Controls.Add(Me.ucrPnlClusterData)
+        Me.Controls.Add(Me.ucrPnlSelectData)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -36,6 +230,24 @@ Partial Class dlgClusterAnalysis
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Cluster Analysis"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents ucrSelectorClusterData As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents rdoHierarchicalData As RadioButton
+    Friend WithEvents rdoPartitioningData As RadioButton
+    Friend WithEvents ucrPnlClusterData As UcrPanel
+    Friend WithEvents rdoNumericVariables As RadioButton
+    Friend WithEvents rdoDataFrame As RadioButton
+    Friend WithEvents ucrPnlSelectData As UcrPanel
+    Friend WithEvents ucrReceiverClusterData As ucrReceiverMultiple
+    Friend WithEvents ucrBase As ucrButtons
+    Friend WithEvents ucrChkStand As ucrCheck
+    Friend WithEvents ucrChkMethod As ucrCheck
+    Friend WithEvents ucrChkCluster As ucrCheck
+    Friend WithEvents ucrChkMetric As ucrCheck
+    Friend WithEvents ucrInputMetric As ucrInputComboBox
+    Friend WithEvents ucrInputMethod As ucrInputComboBox
+    Friend WithEvents ucrNudPamCluster As ucrNud
 End Class
--- instat/dlgClusterAnalysis.vb
+++ instat/dlgClusterAnalysis.vb
@@ -1,3 +1,96 @@
-﻿Public Class dlgClusterAnalysis
+﻿' R- Instat
+' Copyright (C) 2015-2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Imports instat.Translations
+Public Class dlgClusterAnalysis
+    Private bFirstLoad As Boolean = True
+    Private bReset As Boolean = True
+    Private Sub dlgClusterAnalysis_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstLoad Then
+            InitialiseDialog()
+            bFirstLoad = False
+        End If
+        If bReset Then
+            SetDefaults()
+        End If
+        SetRCodeForControls(bReset)
+        bReset = False
+        autoTranslate(Me)
+        TestOkEnabled()
+    End Sub
+    Private Sub InitialiseDialog()
+        Dim dctMetric As New Dictionary(Of String, String)
+        Dim dctMethod As New Dictionary(Of String, String)
+
+        ucrReceiverClusterData.SetParameterIsRFunction()
+        ucrReceiverClusterData.Selector = ucrSelectorClusterData
+        ucrReceiverClusterData.SetMeAsReceiver()
+        ucrReceiverClusterData.SetDataType("numeric")
+
+        ucrSelectorClusterData.SetParameterIsrfunction()
+
+        ucrPnlClusterData.AddRadioButton(rdoPartitioningData)
+        ucrPnlClusterData.AddRadioButton(rdoHierarchicalData)
+        ucrPnlClusterData.AddFunctionNamesCondition(rdoPartitioningData, {"pam"})
+        ucrPnlClusterData.AddFunctionNamesCondition(rdoHierarchicalData, {"agnes"})
+
+        ucrInputMetric.SetParameter(New RParameter("metric", 3))
+        dctMetric.Add("euclidean", Chr(34) & "euclidean" & Chr(34))
+        dctMetric.Add("manhattan", Chr(34) & "manhattan" & Chr(34))
+        ucrInputMetric.SetItems(dctMetric)
+        ucrInputMetric.SetRDefault(Chr(34) & "euclidean" & Chr(34))
+
+        ucrChkMetric.SetText("Metric:")
+        ucrChkMetric.AddToLinkedControls(ucrInputMetric, {True}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrChkMetric.AddFunctionNamesCondition(True, "pam")
+
+
+        ucrPnlSelectData.AddRadioButton(rdoNumericVariables)
+        ucrPnlSelectData.AddRadioButton(rdoDataFrame)
+
+        ucrChkStand.SetParameter(New RParameter("stand", 4))
+        ucrChkStand.SetValuesCheckedAndUnchecked("TRUE", "FALSE")
+        ucrChkStand.SetRDefault("FALSE")
+        ucrChkStand.SetText("Stand:")
+
+        ucrInputMethod.SetParameter(New RParameter("method"), 4)
+        dctMethod.Add("average", Chr(34) & "average" & Chr(34))
+        dctMethod.Add("single", Chr(34) & "single" & Chr(34))
+        dctMethod.Add("complete", Chr(34) & "complete" & Chr(34))
+        dctMethod.Add("ward", Chr(34) & "ward" & Chr(34))
+        dctMethod.Add("weighted", Chr(34) & "weighted" & Chr(34))
+        dctMethod.Add("flexible", Chr(34) & "flexible" & Chr(34))
+        ucrInputMethod.SetItems(dctMethod)
+        ucrInputMethod.SetRDefault(Chr(34) & "average" & Chr(34))
+
+        ucrChkMethod.SetText("Method:")
+        ucrChkMethod.AddToLinkedControls(ucrInputMethod, {True}, bNewLinkedAddRemoveParameter:=True, bNewLinkedHideIfParameterMissing:=True)
+        ucrChkMethod.AddFunctionNamesCondition(True, "pam")
+
+        ucrPnlClusterData.AddToLinkedControls(ucrChkMethod, {rdoHierarchicalData}, bNewLinkedHideIfParameterMissing:=True, bNewLinkedChangeToDefaultState:=True, objNewDefaultState:=False)
+        ucrPnlClusterData.AddToLinkedControls(ucrPnlClusterData, {rdoPartitioningData}, bNewLinkedHideIfParameterMissing:=True, bNewLinkedChangeToDefaultState:=True, objNewDefaultState:=False)
+
+    End Sub
+    Private Sub SetDefaults()
+
+    End Sub
+    Private Sub SetRCodeForControls(bReset As Boolean)
+
+    End Sub
+    Private Sub TestOkEnabled()
+
+    End Sub
 End Class
